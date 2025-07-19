eb38c91ad242405da43eff4fde643318c1a251c9
Rafigzada <139594603+Rafigzada@users.noreply.github.com>
2025-07-19 22:25:28 +0200

    apiChanged

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -35,7 +35,7 @@ export default function LoginPage() {
     setError("")
 
     try {
-        const response = await fetch("http://127.0.0.1:5000/login", {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
